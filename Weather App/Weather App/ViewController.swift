896434e2b653aab34d4c05f0345e33c47cf3a092
Alexandre Marques <alexandreacm.developer@gmail.com>
2024-11-30 20:14:15 -0300

    Added new files

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
+let width = UIScreen.main.bounds.width;
+let height = UIScreen.main.bounds.height;
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 50, width: width / 2, height: height / 2))
+        
+        label.font = UIFont(name: "Arial", size: 30)
+        label.text = "First Application"
+        
+        self.view.addSubview(label)
     }
 
 
